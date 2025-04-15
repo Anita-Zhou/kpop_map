a46819cafcd70247a07afc5f30f7a37f41bd3a16
Anita-Zhou-1 <1011433458@qq.com>
2025-04-15 23:49:15 +0800

    running backend

--- kpop_map_backend/app.js
+++ kpop_map_backend/app.js
@@ -10,8 +10,11 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 app.use('/api/events', eventRoutes);
 
-const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+    res.send('Backend running!');
+  });
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
